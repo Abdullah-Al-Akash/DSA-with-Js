417d3b4af8f691f751502d6347460ef954e66c69
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-11-25 23:22:28 +0600

    Update kth Positive added

--- Order Agnostic Binary Search/kthPostive.js
+++ Order Agnostic Binary Search/kthPostive.js
@@ -1,5 +1,23 @@
-
+let arr = [1, 2, 3, 4];
+let k = 2
 
 function kthPos(arr, k) {
+        let mA = [];
 
+        let n = 1;
+        let i = 0;
+        while (true) {
+                if (n === arr[i]) {
+                        i++;
+                        n++;
+                }
+                else {
+                        mA.push(n);
+                        n++;
+
+                        if (mA.length > k) break;
+                }
+        }
+        return mA[k - 1];
 }
+console.log(kthPos(arr, k));
